3dc7546c0550aa0be37d82f6bcd4a8e8c31029b3
pguilloux <pguilloux53@gmail.com>
2014-01-22 11:08:09 +0100

    recherche simple implémentée (seul le champ boisson/bar fonctionne), on peut rechercher par parties de mots

--- drinks/search_indexes.py
+++ drinks/search_indexes.py
@@ -4,12 +4,25 @@ from drinks.models import *
 
 
 class DrinkIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, use_template=True)
+    text = indexes.NgramField(document=True, use_template=True)
     #description = indexes.CharField(model_attr='description')
     #subcategory = indexes.DateTimeField(model_attr='pub_date')
 
     def get_model(self):
         return Drink
+
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects#.filter(pub_date__lte=datetime.datetime.now())
+
+
+class DrinkCategoryIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.NgramField(document=True, use_template=True)
+    #description = indexes.CharField(model_attr='description')
+    #subcategory = indexes.DateTimeField(model_attr='pub_date')
+
+    def get_model(self):
+        return DrinkCategory
 
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
