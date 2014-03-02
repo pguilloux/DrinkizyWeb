09e1b14b51b0747c9e45259392082cbd9b163e74
pguilloux <pguilloux53@gmail.com>
2014-03-02 18:22:48 +0100

    tentative d'affichage des images avec easy-thumbnail

--- DrinkizyWeb/settings.py
+++ DrinkizyWeb/settings.py
@@ -14,10 +14,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 PROJECT_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),'../')
 
 HAYSTACK_CONNECTIONS = {
-    'default': {
-        'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
-        'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
-    },
+	'default': {
+		'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+		'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
+	},
 }
 
 HAYSTACK_SEARCH_RESULTS_PER_PAGE = 50
@@ -47,7 +47,7 @@ ALLOWED_HOSTS = []
 #AUTH_USER_MODEL = 'users.CustomUser'
 
 ADMINS = (
-    #('Drinkizy', 'pguilloux53@gmail.com'),
+	#('Drinkizy', 'pguilloux53@gmail.com'),
 )
 
 
@@ -58,39 +58,40 @@ AUTH_USER_MODEL = 'users.CustomUser'
 # Application definition
 
 INSTALLED_APPS = (
-    'suit',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+	'suit',
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
 
-    #added packages
-    'south',
-    'haystack',
-    'floppyforms',
-    #'endless_pagination',
-    'autocomplete_light',
-    'tastypie',
-    'sorl.thumbnail',
+	#added packages
+	'south',
+	'haystack',
+	'floppyforms',
+	#'endless_pagination',
+	'autocomplete_light',
+	'tastypie',
+	#'sorl.thumbnail',
+	'easy_thumbnails',
 
-    #apps
-    'home',
-    'bars',
-    'drinks',
-    'search',
-    'users',
-    #'ranks',
+	#apps
+	'home',
+	'bars',
+	'drinks',
+	'search',
+	'users',
+	#'ranks',
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'DrinkizyWeb.urls'
@@ -111,15 +112,15 @@ WSGI_APPLICATION = 'DrinkizyWeb.wsgi.application'
 # }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'Drinkizy',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+		'NAME': 'Drinkizy',                      # Or path to database file if using sqlite3.
+		# The following settings are not used with sqlite3:
+		'USER': 'root',
+		'PASSWORD': '',
+		'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+		'PORT': '',                      # Set to empty string for default.
+	}
 }
 
 # Internationalization
@@ -149,68 +150,86 @@ MEDIA_URL = '/static/medias/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_PATH, 'static/'),
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+	os.path.join(PROJECT_PATH, 'static/'),
+	# Put strings here, like "/home/html/static" or "C:/www/django/static".
+	# Always use forward slashes, even on Windows.
+	# Don't forget to use absolute paths, not relative paths.
 )
 
 TEMPLATE_DIRS = (
-    os.path.join(PROJECT_PATH, 'templates'),
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+	os.path.join(PROJECT_PATH, 'templates'),
+	# Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+	# Always use forward slashes, even on Windows.
+	# Don't forget to use absolute paths, not relative paths.
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = TCP + (
-    'django.core.context_processors.request',
+	'django.core.context_processors.request',
 )
 
+# def get_cache(): 
+# 	import os 
+# 	try: 
+# 		os.environ['MEMCACHE_SERVERS'] = os.environ['MEMCACHIER_SERVERS'].replace(',', ';') 
+# 		os.environ['MEMCACHE_USERNAME'] = os.environ['MEMCACHIER_USERNAME'] 
+# 		os.environ['MEMCACHE_PASSWORD'] = os.environ['MEMCACHIER_PASSWORD'] 
+# 		return {
+# 			'default': {
+# 				'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache', 
+# 				'TIMEOUT': 500, 
+# 				'BINARY': True, 
+# 				'OPTIONS': { 'tcp_nodelay': True } 
+# 			}
+# 		} 
+# 	except: 
+# 		return { 
+# 			'default': { 
+# 				'BACKEND': 'django.core.cache.backends.locmem.LocMemCache' 
+# 			} 
+# 		} 
+# 		CACHES = get_cache() 
+# 		SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer' 
+# 		SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
-        'LOCATION': 'cache_table',
-    }
-}
+
 
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        },
-        'console':{
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler'
-        },
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-        'search.forms': {
-            'handlers': ['console'],
-            'level': 'INFO'
-        },
-        'search.views': {
-            'handlers': ['console'],
-            'level': 'INFO'
-        },
-        'drinks.search_indexes': {
-            'handlers': ['console'],
-            'level': 'INFO'
-        },
+	'version': 1,
+	'disable_existing_loggers': False,
+	'filters': {
+		'require_debug_false': {
+			'()': 'django.utils.log.RequireDebugFalse'
+		}
+	},
+	'handlers': {
+		'mail_admins': {
+			'level': 'ERROR',
+			'filters': ['require_debug_false'],
+			'class': 'django.utils.log.AdminEmailHandler'
+		},
+		'console':{
+			'level': 'DEBUG',
+			'class': 'logging.StreamHandler'
+		},
+	},
+	'loggers': {
+		'django.request': {
+			'handlers': ['mail_admins'],
+			'level': 'ERROR',
+			'propagate': True,
+		},
+		'search.forms': {
+			'handlers': ['console'],
+			'level': 'INFO'
+		},
+		'search.views': {
+			'handlers': ['console'],
+			'level': 'INFO'
+		},
+		'drinks.search_indexes': {
+			'handlers': ['console'],
+			'level': 'INFO'
+		},
 
-    }
+	}
 }
