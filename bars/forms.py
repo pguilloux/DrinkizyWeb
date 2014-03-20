d759c5598054c8d8dd9cf7cb802393137cdb71a8
pguilloux <pguilloux53@gmail.com>
2014-03-20 23:43:20 +0100

    début de formulaire pour la notation d'un bar sur la fiche bar

---
+++ bars/forms.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from django import forms
+from drinks.models import *
+from django.forms.widgets import *
+
+from bars.models import *
+import logging, operator
+
+ 
+logger = logging.getLogger(__name__)
+
+class RankBarForm(forms.ModelForm):
+    class Meta:
+        model = RankBar
+        fields = ['rank']
--- bars/views.py
+++ bars/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from bars.models import *
+from bars.forms import *
 from drinks.models import *
 from haystack.query import SearchQuerySet
 
@@ -9,6 +10,15 @@ from haystack.query import SearchQuerySet
 def fiche_bar(request, slug): 
 	bar = Bar.objects.get(slug=slug)
 	drinks_in_bar = DrinkBar.objects.filter(bar__slug=slug)
+
+	if request.method == 'POST':
+		rank_form = RankBarForm(request.POST) # A form bound to the POST data
+
+		if rank_form.is_valid(): # All validation rules pass
+			rank = rank_form.save()
+
+	else:
+		rank_form = RankBarForm() # An unbound form
 
 	return render_to_response('bars/fiche_bar.html', {'bar': bar, 'drinks_in_bar': drinks_in_bar}, context_instance=RequestContext(request))
 
